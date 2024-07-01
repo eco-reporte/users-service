8a66424f7167c676209199991f191076f9b51de4
migue <211218@ids.upchiapas.edu.mx>
2024-07-01 14:48:08 -0600

    ADD method cors

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import bodyParser from 'body-parser';
 import userRoutes from './users/interfaces/routes/UserRoutes';
 import { sequelize } from './users/infrastructure/orm/sequelize';
 import authRoutes from './auth/infraestructure/Routes/authRoutes';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors()); // Add CORS middleware
 
 app.use('/login', authRoutes);
-app.use('/user', userRoutes); // Agrega las rutas del usuario
+app.use('/user', userRoutes);
 
 sequelize.sync().then(() => {
     app.listen(3001, () => {

b6fbb935741149ab826670ec9c73655af6cba80d
migue <211218@ids.upchiapas.edu.mx>
2024-07-21 23:05:52 -0600

    [UPDATE] json is ok and userRoot is changed

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, {Application, Request, Response} from "express";
 import bodyParser from 'body-parser';
-import userRoutes from './users/interfaces/routes/UserRoutes';
+import userRoutes from './users/interfaces/routes/userRoutes';
 import sequelize from './database/database';
 import authRoutes from './auth/infraestructure/Routes/authRoutes';
 import cors from 'cors';
